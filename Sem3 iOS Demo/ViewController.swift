fe3cee57772a785f798610b0bb3edf1c034374ab
Siddharthan <siddharthan64@users.noreply.github.com>
2017-07-16 23:50:48 -0700

    Initial Commit

---
+++ Sem3 iOS Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sem3 iOS Demo
+//
+//  Created by Siddharthan Asokan on 16/7/17.
+//  Copyright © 2017 Social Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
